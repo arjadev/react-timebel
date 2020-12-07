53f157f06b73a9632129c393ba25f82f3e9ef483
nessaji <40970351+nessaji@users.noreply.github.com>
2020-12-07 14:51:29 +0800

    Initial commit

---
+++ src/Routes.js
@@ -0,0 +1,210 @@
+/* eslint-disable react/no-array-index-key */
+import React, {
+  lazy,
+  Suspense,
+  Fragment
+} from 'react';
+import {
+  Switch,
+  Redirect,
+  Route
+} from 'react-router-dom';
+import DashboardLayout from 'src/layouts/DashboardLayout';
+import MainLayout from 'src/layouts/MainLayout';
+import HomeView from 'src/views/pages/HomeView';
+import LoadingScreen from 'src/components/LoadingScreen';
+import AuthGuard from 'src/components/AuthGuard';
+
+const routesConfig = [
+  {
+    exact: true,
+    path: '/',
+    component: () => <Redirect to="/home" />
+  },
+  {
+    exact: true,
+    path: '/404',
+    component: lazy(() => import('src/views/pages/Error404View'))
+  },
+  {
+    exact: true,
+    path: '/login-unprotected',
+    component: lazy(() => import('src/views/auth/LoginView'))
+  },
+  {
+    exact: true,
+    path: '/register-unprotected',
+    component: lazy(() => import('src/views/auth/RegisterView'))
+  },
+  {
+    path: '/app',
+    guard: AuthGuard,
+    layout: DashboardLayout,
+    routes: [
+      {
+        exact: true,
+        path: '/app',
+        component: () => <Redirect to="/app/reports/dashboard" />
+      },
+      {
+        exact: true,
+        path: '/app/account',
+        component: lazy(() => import('src/views/pages/AccountView'))
+      },
+      {
+        exact: true,
+        path: '/app/reports/dashboard',
+        component: lazy(() => import('src/views/reports/DashboardView'))
+      },
+      {
+        exact: true,
+        path: '/app/reports/dashboard-alternative',
+        component: lazy(() => import('src/views/reports/DashboardAlternativeView'))
+      },
+      {
+        exact: true,
+        path: '/app/reports',
+        component: () => <Redirect to="/app/reports/dashboard" />
+      },
+      {
+        exact: true,
+        path: '/app/management/customers',
+        component: lazy(() => import('src/views/management/CustomerListView'))
+      },
+      {
+        exact: true,
+        path: '/app/management/customers/:customerId',
+        component: lazy(() => import('src/views/management/CustomerDetailsView'))
+      },
+      {
+        exact: true,
+        path: '/app/management/customers/:customerId/edit',
+        component: lazy(() => import('src/views/management/CustomerEditView'))
+      },
+      {
+        exact: true,
+        path: '/app/management/products',
+        component: lazy(() => import('src/views/management/ProductListView'))
+      },
+      {
+        exact: true,
+        path: '/app/management/products/create',
+        component: lazy(() => import('src/views/management/ProductCreateView'))
+      },
+      {
+        exact: true,
+        path: '/app/management/orders',
+        component: lazy(() => import('src/views/management/OrderListView'))
+      },
+      {
+        exact: true,
+        path: '/app/management/orders/:orderId',
+        component: lazy(() => import('src/views/management/OrderDetailsView'))
+      },
+     
+      {
+        exact: true,
+        path: '/app/management',
+        component: () => <Redirect to="/app/management/customers" />
+      },
+      {
+        exact: true,
+        path: '/app/projects/overview',
+        component: lazy(() => import('src/views/projects/OverviewView'))
+      },
+      {
+        exact: true,
+        path: '/app/projects/browse',
+        component: lazy(() => import('src/views/projects/ProjectBrowseView'))
+      },
+      {
+        exact: true,
+        path: '/app/projects/create',
+        component: lazy(() => import('src/views/projects/ProjectCreateView'))
+      },
+      {
+        exact: true,
+        path: '/app/projects/:id',
+        component: lazy(() => import('src/views/projects/ProjectDetailsView'))
+      },
+      {
+        exact: true,
+        path: '/app/projects',
+        component: () => <Redirect to="/app/projects/browse" />
+      },
+      {
+        exact: true,
+        path: '/app/social/feed',
+        component: lazy(() => import('src/views/social/FeedView'))
+      },
+      {
+        exact: true,
+        path: '/app/social/profile',
+        component: lazy(() => import('src/views/social/ProfileView'))
+      },
+      {
+        exact: true,
+        path: '/app/social',
+        component: () => <Redirect to="/app/social/profile" />
+      },
+      {
+        exact: true,
+        path: '/app/extra/charts/apex',
+        component: lazy(() => import('src/views/extra/charts/ApexChartsView'))
+      },
+      {
+        component: () => <Redirect to="/404" />
+      }
+    ]
+  },
+  {
+
+    path: '*',
+    layout: MainLayout,
+    routes: [
+      {
+        exact: true,
+        path: '/home',
+        component: HomeView
+      },
+      {
+        component: () => <Redirect to="/404" />
+      }
+    ]
+  }
+];
+
+const renderRoutes = (routes) => (routes ? (
+  <Suspense fallback={<LoadingScreen />}>
+    <Switch>
+      {routes.map((route, i) => {
+        const Guard = route.guard || Fragment;
+        const Layout = route.layout || Fragment;
+        const Component = route.component;
+
+        return (
+          <Route
+            key={i}
+            path={route.path}
+            exact={route.exact}
+            render={(props) => (
+              <Guard>
+                <Layout>
+                  {route.routes
+                    ? renderRoutes(route.routes)
+                    : <Component {...props} />}
+                </Layout>
+              </Guard>
+            )}
+          />
+        );
+      })}
+    </Switch>
+  </Suspense>
+) : null);
+
+function Routes() {
+  return renderRoutes(routesConfig);
+}
+
+export default Routes;
